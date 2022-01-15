2c8aa97234298d4cf8cc9ee39e83be823660f092
Umesh Taily <71640188+190280107146@users.noreply.github.com>
2022-01-15 12:00:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
     element = Array.from(document.getElementsByClassName('songItemPlay'))[songIndex];
     element.classList.remove('fa-play-circle');
     element.classList.add('fa-pause-circle');
-    audioElement.src = `/spotifyClone-html-css-javaScript//${songIndex+1}.mp3`;
+    audioElement.src = `/spotifyClone-html-css-javaScript/${songIndex+1}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
